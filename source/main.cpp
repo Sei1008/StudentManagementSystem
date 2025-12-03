520f528678c41d714a35b63cdd51bd48a63ec6f3
hao nhien <nhien3043@gmail.com>
2025-12-03 12:35:26 +0700

    Add load and save into main

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,17 @@
 #include "../include/struct.h"
+#include "../include/operations.h"
 #include <iostream>
 
 int main(){
-    
+    //load data
+    load_user_from_csv(users_list);
+    load_unit_from_csv(units_list);
+    load_enrollment_from_csv(enrollments_list);
+    //main menu
+
+
+    //save data
+    save_all_data();
 
     return 0;
 }
