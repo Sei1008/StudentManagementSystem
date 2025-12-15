6b30b2b753a62680869051dbb8bff14c12bbec03
hao nhien <nhien3043@gmail.com>
2025-12-15 22:31:06 +0700

    test

--- source/unit_management.cpp
+++ source/unit_management.cpp
@@ -10,10 +10,10 @@ void list_all_teaching_units_by_teacher_id(int teacher_id){
     cout << "========================================" << endl;
     cout << "   LIST OF ALL YOUR TEACHING UNITS   " << endl;
     cout << "========================================" << endl;
-    cout << setw(5) <<"Unit ID"<< setw(10)<<"Unit Code"<<setw(10)<<"Unit Name"<<endl;
+    cout << left << setw(5) <<"Unit ID"<< setw(10)<<"Unit Code"<<setw(10)<<"Unit Name"<<endl;
     for(const Unit& unit:units_list){
         if(unit.teacher_id==teacher_id){
-            cout << setw(5) <<unit.unit_id<< setw(10)<<unit.unit_code<<setw(10)<<unit.unit_name<<endl;
+            cout << left << setw(5) <<unit.unit_id<< setw(10)<<unit.unit_code<<setw(10)<<unit.unit_name<<endl;
         }
     }
 }
